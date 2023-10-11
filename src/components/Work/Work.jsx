2bc7a974e777641072f839fbdba4520d3a6bc3d4
snipc <snipc.mail@proton.me>
2023-10-11 13:22:02 -0400

    Add projects prototype

--- src/components/Work/Work.jsx
+++ src/components/Work/Work.jsx
@@ -1,14 +1,53 @@
 import React from 'react';
 import './Work.css'
 import ss from '../../assets/ss.png'
+
+
 function Work() {
   return (
  <>
  <div id="repos_container">
 
-    {/* <div className="repo">c</div>
-    <div className="repo">d</div>
-    <div className="repo">e</div> */}
+ <div className="repo">
+      <img src='../../../src/assets/ss.png' alt="" height='250px' width='500px'/>
+      <div id="text">
+      <h2>snipc.me</h2>
+      <p>All new personal website including anime, tools, technologies tab! Written in React.js using Vite (in dev!)</p>
+      </div>
+
+    </div>
+    <div className="repo">
+      <img src='../../../src/assets/ss.png' alt="" height='250px' width='500px'/>
+      <div id="text">
+      <h2>snipc.me</h2>
+      <p>All new personal website including anime, tools, technologies tab! Written in React.js using Vite (in dev!)</p>
+      </div>
+
+    </div>
+    <div className="repo">
+      <img src='../../../src/assets/ss.png' alt="" height='250px' width='500px'/>
+      <div id="text">
+      <h2>snipc.me</h2>
+      <p>All new personal website including anime, tools, technologies tab! Written in React.js using Vite (in dev!)</p>
+      </div>
+    </div>
+    <div className="repo">
+      <img src='../../../src/assets/ss.png' alt="" height='250px' width='500px'/>
+      <div id="text">
+      <h2>snipc.me</h2>
+      <p>All new personal website including anime, tools, technologies tab! Written in React.js using Vite (in dev!)</p>
+      </div>
+
+    </div>
+
+{/*     
+    <div className="repo">
+      <img src={ss} alt="" />
+      <div id="text">
+      <h2>snipc.me</h2>
+      <p>All new personal website including anime, tools, technologies tab! Written in React.js using Vite (in dev!)</p>
+      </div>
+    </div> */}
  </div>
  </>
   )
