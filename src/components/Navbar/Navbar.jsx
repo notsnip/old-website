28d08ceb32c606968d1ba504b8a26e26d382a2fd
snipc <snipc.mail@proton.me>
2023-10-12 10:50:58 -0400

    Change navbar web icon

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,16 +4,16 @@ import './Navbar.css'
 
 
 function Navbar(props) {
-  function redirect() {
+  function redirect(location) {
     console.log('running')
-    window.location= "http://old.snipc.me";
+    window.location= `http://${location}`;
   }
 
   return (
     // navbar section 
     <div id='navbar'>
         <span id='mode' target=''><iconify-icon icon="ph:sun" height='1.4em'></iconify-icon></span>
-        <span id='mode' target=''><iconify-icon icon="mdi:web" height='1.4em'  onClick={redirect}></iconify-icon></span>
+        <span id='mode' target=''><iconify-icon icon="ic:round-home" height='1.4em'  onClick={redirect('snipc.me')}></iconify-icon></span>
         </div>
 )}
 
