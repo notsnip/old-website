d1d343f85de05d403b14c867bcb467400a83912f
snipc <snipc.mail@proton.me>
2023-10-12 12:09:47 -0400

    Add sound on click

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,17 @@ import Anime from './components/Anime/Anime';
 import Work from './components/Work/Work';
 import AnimatedCursor from "react-animated-cursor";
 import ScrollToTop from './ScrollToTop';
+import sound from './sound.mp3'
 
 
 function App(){
+  function play() {
+    new Audio(sound).play()
+  }
     
   return (
     <>
+    <div id='main_div' onClick={play}>
     <AnimatedCursor color='128, 128, 128'/>
     <Navbar/>
     <ScrollToTop/>
@@ -23,6 +28,7 @@ function App(){
       <Route path='/contact' element={<Contact/>}/>
       <Route path='/work' element={<Work/>}/>
       </Routes>
+      </div>
     </>
   )
 }
