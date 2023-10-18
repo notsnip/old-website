0f3a9724b07311b96f03eb32dc0f49531631c458
snipc <snipc.mail@proton.me>
2023-10-18 04:04:27 -0400

    Update: Initial Commit

--- src/components/Anime/Anime.jsx
+++ src/components/Anime/Anime.jsx
@@ -19,10 +19,11 @@ function Anime() {
   fetch("https://api.jikan.moe/v4/users/snipc/full")
   .then((res) => res.json())
   .then((json) => {
-    let enry_path= json.data.updates.anime
-    // let fav_path= json.data.favorites.anime[0]
 
-    console.log(enry_path)
+    let fav_path= json.data.favorites.anime
+    let enry_path= json.data.updates.anime
+
+    // console.log(enry_path)
     setanime1(enry_path[0].entry.title)
     setimg1(enry_path[0].entry.images.jpg.image_url)
     setep1(enry_path[0].status)
