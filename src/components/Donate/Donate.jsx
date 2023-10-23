6169016533f7aec3edfe501b903d6cb544a268d9
snipc <snipc.mail@proton.me>
2023-10-23 06:04:28 -0400

    Update: add fadein effect to donate section

--- src/components/Donate/Donate.jsx
+++ src/components/Donate/Donate.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import './Donate.css'
+import FadeIn from 'react-fade-in/lib/FadeIn';
 
 function Donate(props) {
   return (
 <>
 <title>{props.type} • snipc</title>
+<FadeIn delay={400}>
 <div id="hero-text">
     <h2>{props.type}</h2>
     <p>{props.title}</p>
@@ -16,6 +18,7 @@ function Donate(props) {
 <iconify-icon icon="simple-icons:buymeacoffee" height='2rem'></iconify-icon>
     <h5>Buy me a coffee</h5>
 </div>
+</FadeIn>
 
 </>
   )
