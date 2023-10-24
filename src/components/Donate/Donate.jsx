513d2e5657b8b15c63e288c2e3676656169b1b69
snipc <snipc.mail@proton.me>
2023-10-24 07:24:52 -0400

    Fix: add link in donate page

--- src/components/Donate/Donate.jsx
+++ src/components/Donate/Donate.jsx
@@ -14,10 +14,12 @@ function Donate(props) {
 
 <div id="donate-methods"></div>
 
+<a href="https://buymeacoffee.com/snipc">
 <div className="donate-method">
 <iconify-icon icon="simple-icons:buymeacoffee" height='2rem'></iconify-icon>
     <h5>Buy me a coffee</h5>
 </div>
+</a>
 </FadeIn>
 
 </>
