a487c8e811c65633910d2b80580bdb03fab0eb7a
snipc <snipc.mail@proton.me>
2023-11-01 11:21:28 -0400

    TestCase 03

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Outlet, Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar/Navbar';
 import Hero from './components/Hero/Hero';
 import Anime from './components/Anime/Anime';
