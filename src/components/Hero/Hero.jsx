37f2cee747b042e350e72b973f28ae1dfc7d7b13
snipc <snipc.mail@proton.me>
2023-11-03 09:00:04 -0400

    Add more apps to stack and fix some problems

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -169,7 +169,7 @@ function Hero(props) {
       <p id='pages_title' style={{ marginTop: '20vh' }}>ABOUT ME</p>
       <div id="about_me">
         <p id="about_para">My name is <span id='Shaurya'>Shaurya</span>. I'm always doing <u>random things</u>.
-          <br /> <br /> Beyond computers, I'm really interested in <a href="/anime" id="about_me_link">anime</a> and <a id='about_me_link' href="/songs">music</a>, you can often catch me spending time with those.
+          <br /> <br /> Beyond computers, I'm really interested in <a href="/anime" id="about_me_link">anime</a> and <a id='about_me_link' href="https://www.last.fm/user/realsnipc">music</a>, you can often catch me spending time with those.
           <br />Or I'll be reading something or doing homework.</p>
         <img src="https://media.giphy.com/media/MyibCKeY7w2TS/giphy.gif" alt="some random anime gif" id='about_me_gif' />
       </div>
--- src/components/Stack/Stack.jsx
+++ src/components/Stack/Stack.jsx
@@ -8,9 +8,9 @@ function Stack() {
         <FadeIn delay={600}>
             <div id="hero_text">
                 <h2 className='text_hero_text'>Stack</h2>
-                <p className='text_hero_text'>From code editors to design tools, I use a variety of software to make things epic
+                <p className='text_hero_text'>From games to design tools, I use a variety of software to make things epic.
                     <br />
-                    (Just don't be surprised if you see fun apps on the list. I'm a geek at heart, after all.)
+                    (This list mentions almost every app I use, not just development tools.)
 
 
                 </p>
@@ -87,6 +87,13 @@ function Stack() {
                         </div>
                         <span className="tech-card-desc">Iconify</span>
                     </div>
+
+                    <div className="technologies-card">
+                        <div className="tech-card-display">
+                            <iconify-icon icon="arcticons:proton-mail" height='1.5rem'></iconify-icon>
+                        </div>
+                        <span className="tech-card-desc">Proton Mail</span>
+                    </div>
                     {/* Template to add more item  */}
                     {/* <div className="technologies-card">
                 <div className="tech-card-display">
@@ -98,7 +105,7 @@ function Stack() {
                 </div>
 
                 <div className="app-container">
-                    <span className="technologies-text tech-sub">APPLICATIONS</span>
+                    <span className="technologies-text tech-sub">DESKTOP APPS (<iconify-icon icon="fa6-brands:fedora"></iconify-icon>)</span>
                 </div>
                 <div className="container2">
 
@@ -115,6 +122,21 @@ function Stack() {
                         <span className="tech-card-desc">Figma</span>
                     </div>
 
+                    <div className="technologies-card">
+                        <div className="tech-card-display">
+                            <iconify-icon icon="logos:chrome" height='1.5rem'></iconify-icon>
+                        </div>
+                        <span className="tech-card-desc">Chrome</span>
+                    </div>
+
+                    <div className="technologies-card">
+                        <div className="tech-card-display">
+                            <iconify-icon icon="logos:discord-icon" height='1.3rem'></iconify-icon>
+                        </div>
+                        <span className="tech-card-desc">Discord</span>
+                    </div>
+
+
                     {/* Template to add more item  */}
                     {/* <div className="technologies-card">
                 <div className="tech-card-display">
@@ -122,13 +144,63 @@ function Stack() {
                 </div>
                 <span className="tech-card-desc">Adobe</span>
             </div> */}
-                    <p className='text_hero_text stack_note'>(this awesome section uses a code snippet from <a href="https://wounds.rip/">neksio</a>)
+                   
+                </div>
+
+                <div className="app-container">
+                    <span className="technologies-text tech-sub">MOBILE APPS (<iconify-icon icon="uil:android"></iconify-icon>)</span>
+                </div>
+                <div className="container2">
+
+                    <div className="technologies-card">
+                        <div className="tech-card-display">
+                            <iconify-icon icon="openmoji:discord" height='1.9rem'></iconify-icon>
+                        </div>
+                        <span className="tech-card-desc">Discord</span>
+                    </div>
+
+                    <div className="technologies-card">
+                        <div className="tech-card-display">
+                            <iconify-icon icon="skill-icons:linkedin" height='1.5rem'></iconify-icon>
+                        </div>
+                        <span className="tech-card-desc">Linkedin</span>
+                    </div>
 
 
-                    </p>
+                    <div className="technologies-card">
+                        <div className="tech-card-display">
+                            <iconify-icon icon="logos:spotify-icon" height='1.5rem'></iconify-icon>
+                        </div>
+                        <span className="tech-card-desc">Spotify</span>
+                    </div>
+
+                    <div className="technologies-card">
+                        <div className="tech-card-display">
+                            <iconify-icon icon="arcticons:anime-slayer" height='1.5rem'></iconify-icon>
+                        </div>
+                        <span className="tech-card-desc">Dantotsu</span>
+                    </div>
+
+                    <div className="technologies-card">
+                        <div className="tech-card-display">
+                            <iconify-icon icon="simple-icons:roadmapdotsh" height='1.5rem'></iconify-icon>
+                        </div>
+                        <span className="tech-card-desc">Roadmap.sh</span>
+                    </div>
+
+                    {/* Template to add more item  */}
+                    {/* <div className="technologies-card">
+                <div className="tech-card-display">
+                <iconify-icon icon="vscode-icons:file-type-html" height='1.5rem'></iconify-icon>
+                </div>
+                <span className="tech-card-desc">Adobe</span>
+            </div> */}
                 </div>
             </div>
+            <p className='text_hero_text stack_note'>(this awesome section uses a code snippet from <a href="https://wounds.rip/">neksio</a>)
 
+
+</p>
             {/* scroll to top button  */}
             <ScrollToTop showUnder={160}>
                 <div id="scroll_to_top">
