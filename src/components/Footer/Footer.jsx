170bdadf486fc245f4e25b78cbdd3a10febfbc1f
realsnipc <vishakhachaudhary19555@gmail.com>
2024-01-01 20:25:50 +0530

    Update Version

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,7 +7,7 @@ function Footer(props) {
 
 <div id="footer">
   <a href='https://github.com/realsnipc/snipc.me' id="link" target='_blank'>
-    <p id='icon'><iconify-icon icon="ph:link-bold" style={{color: '#999'}}></iconify-icon> </p> <span id='text'>Open Source - V3.3.1</span>
+    <p id='icon'><iconify-icon icon="ph:link-bold" style={{color: '#999'}}></iconify-icon> </p> <span id='text'>Open Source | © 2023-2024</span>
     </a>
 </div>
   )
