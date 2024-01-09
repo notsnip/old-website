c0a5db790fe47d274765b868ecb6b816b874a047
realsnipc <snipc.mail@proton.me>
2024-01-09 20:08:09 +0530

    Update footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,7 +7,7 @@ function Footer(props) {
 
 <div id="footer">
   <a href='https://github.com/realsnipc/snipc.me' id="link" target='_blank'>
-    <p id='icon'><iconify-icon icon="ph:link-bold" style={{color: '#999'}}></iconify-icon> </p> <span id='text'>Open Source | © {new Date().getFullYear()}</span>
+    <p id='icon'><iconify-icon icon="ph:link-bold" style={{color: '#999'}}></iconify-icon> </p> <span id='text'>Open Source | MIT License</span>
     </a>
 </div>
   )
